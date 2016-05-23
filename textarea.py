817825ca6818ee371e88d30d3cad01e952f2c7f1
Robin Manoli <RobinManoli@users.noreply.github.com>
2016-05-23 16:34:54 +0200

    First attempt at automating whitespace with key presses: enter, tab and shift-tab

--- textarea.py
+++ textarea.py
@@ -1,7 +1,5 @@
 from tkinter import *
 
-# todo: delete this comment  <html onload="calc(multiply(3,4),multiply(4,multiply(5,multi[2])))">
-
 class TextArea():
     def __init__(self, parent):
         self.parent = parent
@@ -26,11 +24,44 @@ class TextArea():
         textarea.pack(side=LEFT, fill=BOTH, expand=True)
         #textarea.bind('<<Modified>>', self.changed) # works only once
         textarea.bind('<KeyRelease>', self.changed)
-        textarea.bind('<Motion>', self.motion_textarea)
-        textarea.bind('<Enter>', self.enter_textarea)
-        textarea.bind('<Leave>', self.leave_textarea)
+        textarea.bind('<Motion>', self.on_motion)
+        textarea.bind('<Enter>', self.on_enter)
+        textarea.bind('<Leave>', self.on_leave)
+        textarea.bind('<Return>', self.on_return)
+        textarea.bind('<Tab>', self.on_tab)
+        textarea.bind('<Shift-Tab>', self.on_shift_tab)
         self.mainframe.textarea = textarea
 
+    def get_whitespace(self, text):
+        "Finds and returns the whitespace of the beginning of text."
+        whitespace = ''
+        if text != text.lstrip():
+            for c in text:
+                if c.strip():
+                    break
+                whitespace += c
+        return whitespace
+
+    def getline_number(self, index=INSERT):
+        index = self.mainframe.textarea.index(index)
+        nline = int( index.split('.')[0] )
+        return nline
+        
+    def getline_start_end(self, index=INSERT):
+        "Get the start and end indeces of retrieved line number."
+        nline = self.getline_number(index)
+        start = "%d.0" % nline
+        end = "%d.0 - 1c" % (nline + 1)
+        return start, end
+
+    def getline(self, index=INSERT):
+        "Get text content of line index. Index may be a tk text widget index or a line number."
+        integer = 1
+        if type(index) == type(integer):
+            index = str(index) + '.0'
+        start, end = self.getline_start_end(index)
+        return self.mainframe.textarea.get(start, end)
+        
     def scrollY(self, action, position, type=None):
         self.mainframe.textarea.yview_moveto(position)
         self.mainframe.linenumbers.yview_moveto(position)
@@ -46,18 +77,63 @@ class TextArea():
         self.update_linenumbers(event)
         self.mainframe.highlight(event, forced=True)
 
-    def motion_textarea(self, event=None):
+    def on_motion(self, event=None):
         self.mainframe.HighLight.brackets(event)
 
-    def leave_textarea(self, event=None):
+    def on_leave(self, event=None):
         self.do_highlight_whitespace = True
         self.mainframe.highlight(event, forced=True)
 
-
-    def enter_textarea(self, event=None):
+    def on_enter(self, event=None):
         self.do_highlight_whitespace = False
         self.mainframe.highlight(event, forced=True)
 
+    def on_return(self, event=None):
+        "Create whitespace for new lines (same as above line)"
+        line = self.getline()
+        whitespace = self.get_whitespace(line)
+        self.mainframe.textarea.insert(INSERT, '\n' + whitespace)
+        return "break"
+
+    def on_tab(self, event=None):
+        "Add whitespace (same as above line with more)"
+        start, end = self.getline_start_end()
+        if self.mainframe.textarea.get(start, INSERT).lstrip():
+            # normal tab if cursor not in leading whitespace
+            return
+
+        line = self.getline()
+        whitespace = self.get_whitespace(line)
+        nline = self.getline_number()
+        # find nearest above line with lesser whitespace
+        for i in range(nline + 1, self.getline_number('end - 1c')):
+            line2 = self.getline(i)
+            whitespace2 = self.get_whitespace(line2)
+            if len(whitespace) < len(whitespace2):
+                while not self.mainframe.textarea.get(start, start + '+ 1c').lstrip():
+                    self.mainframe.textarea.delete(start, start + '+ 1c')
+                self.mainframe.textarea.insert(start, whitespace2)
+                #print( repr(whitespace2) )
+                return "break"
+
+    def on_shift_tab(self, event=None):
+        "Remove whitespace (same as above line with lesser)"
+        line = self.getline()
+        whitespace = self.get_whitespace(line)
+        if whitespace:
+            nline = self.getline_number()
+            # find nearest above line with lesser whitespace
+            for i in range(nline, 1, -1):
+                line2 = self.getline(i)
+                whitespace2 = self.get_whitespace(line2)
+                if len(whitespace) > len(whitespace2):
+                    start, end = self.getline_start_end()
+                    while not self.mainframe.textarea.get(start, start + '+ 1c').lstrip():
+                        self.mainframe.textarea.delete(start, start + '+ 1c')
+                    self.mainframe.textarea.insert(start, whitespace2)
+                    #print( repr(whitespace2) )
+                    break
+        return "break"
 
     def update_linenumbers(self, event=None):
         # get content, but don't include the last newline inserted by tkinter
