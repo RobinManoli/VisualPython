0d0c89de87ee27a67066a2e1c7fad055201458a4
Robin Manoli <RobinManoli@users.noreply.github.com>
2016-06-01 20:55:48 +0200

    ADDED [un]comment by click to LineNumbers

--- linenumbers.py
+++ linenumbers.py
@@ -14,32 +14,37 @@ class LineNumbers(Canvas):
         self.textarea = textarea
         self.textarea.linenumbers = self
 
-        self.last_motion_index = ''
         self.linenumber_items = []
+        self.linetool_item = None
 
         self.bind('<Motion>', self.on_motion)
-        self.bind('<Double-Button-1>', self.on_dclick)
+        self.bind('<Button-1>', self.on_click)
 
     def on_motion(self, event=None):
-        self.draw()
+        #print(dir(event))
+        coords = '@%d,%d' % (event.x, event.y)
+        bbox = self.textarea.bbox(coords)
+        if bbox:
+            if self.linetool_item:
+                self.delete( self.linetool_item )
+            x, y, width, height = bbox
+            item = self.create_text((30,y), text="#//", anchor=NW)
+            self.linetool_item = item
 
+    def on_click(self, event=None):
+        "Comment or uncomment corresponding line in textarea."
+        coords = '@%d,%d' % (event.x, event.y)
+        index = self.textarea.index(coords)
 
-    def on_dclick(self, event=None):
-        toolline = self.mainframe.texthelper.Line(self, CURRENT)
-        while not toolline.text.strip():
-            # textarea wrapped lines are newlines in linetools, so find the closest above written line number
-            toolline = self.mainframe.texthelper.Line(self, toolline.n - 1)
-        neditorline = int(toolline.text.rstrip('#'))
-        editorline = self.mainframe.texthelper.Line(self.textarea, neditorline)
-
-        if not editorline.text:
+        line = self.textarea.texthelper.Line(self.textarea, index)
+        if not line.text:
             return
 
         #while not self.mainframe.TextArea.get(start, start + '+ 1c').lstrip():
-        if editorline.text.lstrip().startswith('#'):
-            self.textarea.delete(editorline.start + ' + %dc' % len(editorline.indent), editorline.start + '+ %dc + 1c' % len(editorline.indent))
+        if line.text.lstrip().startswith('#'):
+            self.textarea.delete(line.start + ' + %dc' % len(line.indent), line.start + '+ %dc + 1c' % len(line.indent))
         else:
-            self.textarea.insert(editorline.start + ' + %dc' % len(editorline.indent), '#')
+            self.textarea.insert(line.start + ' + %dc' % len(line.indent), '#')
 
     def draw(self, event=None):
         for item in self.linenumber_items:
@@ -48,9 +53,12 @@ class LineNumbers(Canvas):
         bv = self.textarea.texthelper.bottom_visible(self.textarea)
         tvline = self.textarea.texthelper.Line(self.textarea, tv)
         bvline = self.textarea.texthelper.Line(self.textarea, bv)
+        tag = 'LineNumber'
         for i in range(tvline.n, bvline.n + 1):
             bbox = self.textarea.bbox('%d.%d' % (i,tvline.column))
             if bbox:
                 x, y, width, height = bbox
-                item = self.create_text((x,y), text="%d" % i, anchor=NW)
+                item = self.create_text((x,y), text="%d" % i, anchor=NW, tags=tag)
                 self.linenumber_items.append( item )
+        # binds created items only
+        #self.tag_bind(tag, '<Motion>', self.on_motion)
